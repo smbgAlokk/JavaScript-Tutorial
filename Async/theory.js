b66b574ab625dc0822b0757d27ea42eb1f2f0051
PasswordGenerator <contact.alokkumarr@gmail.com>
2025-06-10 17:43:46 +0530

    async talks

---
+++ Async/theory.js
@@ -0,0 +1,22 @@
+/* 
+
+Javascript => 1. Synchronous
+              2. Single threaded
+              
+Execution Context => execute one line of code at a time
+                     Each operation waits for the last one to complete before executing
+
+blocking Code => block the flow of program => Read File Sync
+Non blocking Code => does not block execution => Read File Async
+
+
+
+
+Event Loop:
+
+The event loop is a core concept in JavaScript that enables non-blocking, asynchronous behavior.
+The JavaScript event loop is a fundamental mechanism that enables JavaScript to handle asynchronous 
+operations and maintain a non-blocking execution model, despite being single-threaded.
+It ensures that tasks like user interactions, network requests, and timers are processed efficiently 
+without freezing the main thread. 
+*/
