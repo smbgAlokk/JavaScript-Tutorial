0634492d3359786aa3a1e24ba31a2c08ee65abca
PasswordGenerator <contact.alokkumarr@gmail.com>
2025-06-14 14:37:42 +0530

    random color generator (mini project)

---
+++ 08_events/three.js
@@ -0,0 +1,26 @@
+// Generate a random color
+
+const randomColor = function () {
+  const hex = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += hex[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
+let intervalId;
+const startChangingColor = function () {
+  function start() {
+    document.body.style.backgroundColor = randomColor();
+  }
+  if (!intervalId) {
+    intervalId = setInterval(start, 1000);
+  }
+};
+const stopChangingColor = function () {
+  clearInterval(intervalId);
+  intervalId = null;
+};
+
+document.querySelector("#start").addEventListener("click", startChangingColor);
+document.querySelector("#stop").addEventListener("click", stopChangingColor);
